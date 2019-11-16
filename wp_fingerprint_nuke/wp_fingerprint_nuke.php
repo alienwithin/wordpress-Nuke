2f4106cd09e8480133b3e189081a2b4f1ff80095
Munir Njiru <alienwithin@users.noreply.github.com>
2019-11-16 18:26:54 +0300

    Update wp_fingerprint_nuke.php
    Added Rest API filter for user enumeration.

--- wp_fingerprint_nuke/wp_fingerprint_nuke.php
+++ wp_fingerprint_nuke/wp_fingerprint_nuke.php
@@ -54,3 +54,12 @@ function aliens_check_enumeration($redirect, $request) {
 	if (preg_match('/\?author=([0-9]*)(\/*)/i', $request)) wp_redirect( home_url() ); 
 	else return $redirect;
 }
+add_filter( 'rest_endpoints', function( $endpoints ){
+    if ( isset( $endpoints['/wp/v2/users'] ) ) {
+        unset( $endpoints['/wp/v2/users'] );
+    }
+    if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+        unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+    }
+    return $endpoints;
+});
